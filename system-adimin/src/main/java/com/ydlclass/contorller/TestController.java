488f27c85ef05c3063c75b401471f00de1d78de6
root <2289424711@qq.com>
2022-03-28 07:56:16 +0800

    测试控制层

---
+++ system-adimin/src/main/java/com/ydlclass/contorller/TestController.java
@@ -0,0 +1,13 @@
+package com.ydlclass.contorller;
+
+/**
+ * Project Name: ydl_wssm_2
+ * File Name: TestController
+ *
+ * @version 1.0
+ * @author:wenerduo
+ * @Date: 2022/03/28/上午 7:55
+ * Copyright (c) 2022,
+ */
+public class TestController {
+}
