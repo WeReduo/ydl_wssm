528f9755804cb779a21651b99c0341e2fd2a23e7
root <2289424711@qq.com>
2022-03-27 21:15:11 +0800

    4

--- ydl-ui/src/main.js
+++ ydl-ui/src/main.js
@@ -2,7 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from '@/router'
 import store from '@/store'
-
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 let app=createApp(App);
-
-app.use(router).use(store).mount('#app')
+//全局安装路由组件
+app.use(router).use(store).use(ElementPlus).mount('#app')
